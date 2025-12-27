3941845de9bff1c010809b70c3b67dd75f18e1b5
Antopablo <lefevreanto57@gmail.com>
2025-12-27 23:42:17 +0100

    Save Wallet + Identity

--- internal/blockchain/database/identity_db.go
+++ internal/blockchain/database/identity_db.go
@@ -2,10 +2,30 @@ package database
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/TookenOrg/tooken-services/internal/globals"
 	"github.com/ethereum/go-ethereum/common"
 )
 
 func GetIdentityAddrByUserId(ctx context.Context, userId int) (identityAddr *common.Address, err error) {
 	return &common.MaxAddress, nil
+}
+
+func InsertIdentity(ctx context.Context, userId int, walletId int64, identityAddress, identityTxHash string) (err error) {
+	query := `
+        INSERT INTO blk.identity
+            (user_id, wallet_id, address, tx_hash)
+        VALUES ($1, $2, $3, $4)
+		RETURNING id
+    `
+
+	var identityID int64
+	err = globals.DB.QueryRow(query, userId, walletId, identityAddress, identityTxHash).Scan(&identityID)
+	if err != nil {
+		return fmt.Errorf("failed to insert wallet: %w", err)
+	}
+
+	fmt.Printf("Identity inserted with id: %d\n", identityID)
+	return
 }
--- internal/blockchain/database/wallet_db.go
+++ internal/blockchain/database/wallet_db.go
@@ -2,12 +2,36 @@ package database
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/TookenOrg/tooken-services/internal/blockchain/utils"
+	"github.com/TookenOrg/tooken-services/internal/globals"
 	"github.com/ethereum/go-ethereum/common"
 )
 
 func GetWalletByUserId(ctx context.Context, userId int) (walletPubKey *common.Address, err error) {
 	// TODO MOCK
+	walletPubKey = &common.MaxAddress
+	return
+}
 
+func InsertWallet(userID int, walletAddress, label, privateKeyClear string) (id int64, err error) {
+	encryptedKey, err := utils.EncryptAESGCM(privateKeyClear)
+	if err != nil {
+		return 0, fmt.Errorf("failed to encrypt private key: %w", err)
+	}
+
+	query := `
+        INSERT INTO blk.user_wallet
+            (user_id, wallet_address, label, private_key_clear, private_key_encrypted)
+        VALUES ($1, $2, $3, $4, $5)
+        RETURNING id
+    `
+	err = globals.DB.QueryRow(query, userID, walletAddress, label, privateKeyClear, encryptedKey).Scan(&id)
+	if err != nil {
+		return 0, fmt.Errorf("failed to insert wallet: %w", err)
+	}
+
+	fmt.Printf("Wallet inserted with id: %d\n", id)
 	return
 }
